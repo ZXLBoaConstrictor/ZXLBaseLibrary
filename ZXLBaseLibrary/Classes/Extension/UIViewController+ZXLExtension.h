610542bca4bec1bbf437b145eb4137a3260582a1
zhangxiaolong <zhangxiaolong@zhixuezhen.com>
2019-01-15 12:00:34 +0800

    添加系统返回按钮事件获取

--- ZXLBaseLibrary/Classes/Extension/UIViewController+ZXLExtension.h
+++ ZXLBaseLibrary/Classes/Extension/UIViewController+ZXLExtension.h
@@ -6,8 +6,13 @@
 //
 
 #import <UIKit/UIKit.h>
+@protocol ZXLBackButtonHandlerProtocol <NSObject>
+@optional
+// Override this method in UIViewController derived class to handle 'Back' button click
+-(BOOL)navigationShouldPopOnBackButton;
+@end
 
-@interface UIViewController (ZXLExtension)
+@interface UIViewController (ZXLExtension)<ZXLBackButtonHandlerProtocol>
 - (void)creatTitleBtn:(UIBarButtonSystemItem)systemItem
                 image:(UIImage *)image
                string:(NSString *)string;
--- ZXLBaseLibrary/Classes/Extension/UIViewController+ZXLExtension.m
+++ ZXLBaseLibrary/Classes/Extension/UIViewController+ZXLExtension.m
@@ -26,3 +26,37 @@
 }
 
 @end
+
+//https://github.com/onegray/UIViewController-BackButtonHandler
+
+@implementation UINavigationController (ZXLExtension)
+
+- (BOOL)navigationBar:(UINavigationBar *)navigationBar shouldPopItem:(UINavigationItem *)item {
+    
+    if([self.viewControllers count] < [navigationBar.items count]) {
+        return YES;
+    }
+    
+    BOOL shouldPop = YES;
+    UIViewController* vc = [self topViewController];
+    if([vc respondsToSelector:@selector(navigationShouldPopOnBackButton)]) {
+        shouldPop = [vc navigationShouldPopOnBackButton];
+    }
+    
+    if(shouldPop) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self popViewControllerAnimated:YES];
+        });
+    } else {
+        for(UIView *subview in [navigationBar subviews]) {
+            if(0. < subview.alpha && subview.alpha < 1.) {
+                [UIView animateWithDuration:.25 animations:^{
+                    subview.alpha = 1.;
+                }];
+            }
+        }
+    }
+    return NO;
+}
+
+@end
